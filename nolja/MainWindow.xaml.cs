dcd0f8bdf27758ea02ef4b4b0821f3f1182aff72
Gima_Home <gimapei@gmail.com>
2016-06-17 00:26:35 +0900

    1

--- nolja/MainWindow.xaml.cs
+++ nolja/MainWindow.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -15,19 +16,66 @@ using System.Windows.Shapes;
 
 namespace nolja
 {
+    public class Nickname : INotifyPropertyChanged
+    {
+        public event System.ComponentModel.PropertyChangedEventHandler PropertyChanged;
+        void Notify(string propName)
+        {
+            if(PropertyChanged != null)
+            {
+                PropertyChanged(this, new PropertyChangedEventArgs(propName));
+            }
+        }
+
+        string name;
+        string nick;
+
+        public string Name
+        {
+            get { return this.name; }
+            set { this.name = value;
+                Notify("Name");
+            }
+        }
+        public string Nick
+        {
+            get { return this.nick; }
+            set
+            {
+                this.nick = value;
+                Notify("Nick");
+            }
+        }
+
+        public Nickname() : this("name", "nick") { }
+        public Nickname(string na, string ni)
+        {
+            this.name = na;
+            this.nick = ni;
+        }
+    }
+
+    public class Nicknames : System.Collections.ObjectModel.ObservableCollection<Nickname> { }
     /// <summary>
     /// Interaction logic for MainWindow.xaml
     /// </summary>
     public partial class MainWindow : Window
     {
+        Nicknames names;
+
         public MainWindow()
         {
             InitializeComponent();
+            this.btnAdd.Click += btnAdd_Click;
+
+            this.names = new Nicknames();
+
+            sPanel.DataContext = this.names;
         }
 
         private void btnAdd_Click(object sender, RoutedEventArgs e)
         {
-
+            this.names.Add(new Nickname());
         }
     }
 }
